b28efe6fa300e2a247d9a18b3634ecd3207c1e8e
krishsingatkar4 <krishsingatkar4@gmail.com>
2026-07-01 01:42:13 +0530

    Complete Phase 1 - Vehicle Management

--- main.py
+++ main.py
@@ -4,7 +4,60 @@ fault_database = []
 scan_history = []
 
 class Vehicle:
-    pass
+    def __init__(self,vehicle_id,owner_name,company,model,year,fuel_type,vin):
+        self.vehicle_id = vehicle_id
+        self.owner_name = owner_name
+        self.company = company
+        self.model = model
+        self.year = year
+        self.fuel_type = fuel_type
+        self.vin = vin
+         
+    def display_vehicle(self):
+            print(" ========== VEHICLE ========== ")
+            print(f"Vehicle Id : {self.vehicle_id}")
+            print(f"Owner : {self.owner_name}")
+            print(f"Company : {self.company}")
+            print(f"Model : {self.model}")
+            print(f"Year : {self.year}")
+            print(f"Fuel : {self.fuel_type}")
+            print(f"VIN : {self.vin}")
+            print("=============================")
+
+    def update_vehicle(self):
+            pass
+
+def add_vehicle():
+    vehicle_id = input("Enter the ID of the vehicle:- ")
+    owner_name = input("Enter the name of the owner:- ")
+    company = input("Enter the name of the company:- ")
+    model = input("Enter the model of the vehicle:- ")
+    year = input("Enter the year of the vehicle:- ")
+    fuel_type = input("Enter the fuel type of the vehicle:- ")
+    vin = input("Enter the VIN of the vehicle:- ")
+    new_vehicle = Vehicle(vehicle_id,owner_name,company,model,year,fuel_type,vin)
+    vehicles.append(new_vehicle)
+
+def show_all_vehicle():
+    if len(vehicles) == 0:
+        print("No vehicle found in OBD Diagnostic!")
+    else:
+         for vehicle in vehicles:
+              print(" ========== ALL VEHICLES ==========")
+              vehicle.display_vehicle()
+              print()
+
+def search_vehicle():
+    vehicle_model = input("Enter the model of the vehicle which you want to search:- ")
+    for vehicle in vehicles:
+        if vehicle.model.lower().strip() == vehicle_model.lower().strip():
+            print("Vehicle found Successfully!")
+            vehicle.display_vehicle()
+            print()
+            break
+    else:
+        print("Vehicle not found. Please first add your vehicle!")
+        
 
 class ECU:
     pass
